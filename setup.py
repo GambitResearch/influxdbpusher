1ee90e2a303599c7de16cf9f78c97a8779b8c4f9
Gustavo Carneiro <gustavocarneiro@gambitresearch.com>
2018-04-30 11:01:57 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="influxdbpusher",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/GambitResearch/influxdbpusher",
 
     author="Gustavo Carneiro",
